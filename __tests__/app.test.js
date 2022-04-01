5c7b0a8454cd968bb2d3d5d2eba64b1bbd5e4158
Alex Blair <bclan11@gmail.com>
2022-04-01 12:24:33 -0700

    wrote github utils

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,8 +2,9 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const GitHubUser = require('../lib/models/GitHubUser');
 
-jest.mock('../lib/utils/github');
+jest.mock('../lib/utils/__mocks__/github');
 
 describe('gitty routes', () => {
   beforeEach(() => {
@@ -19,5 +20,38 @@ describe('gitty routes', () => {
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
+  });
+
+  it('should login and redirect users to /api/v1/github/posts', async () => {
+    const req = await request
+      .agent(app)
+      .get('api/ve/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(req.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
+  it('should allow logged in user to make a post via POST', async () => {
+    await GitHubUser.insert({
+      username: 'test_user',
+      photoUrl: 'http://image.com/image.png',
+    });
+    return request(app)
+      .post('/api/v1/posts')
+      .send({ text: 'Text Post' })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          text: 'Text Post',
+          username: 'test_user',
+        });
+      });
   });
 });
